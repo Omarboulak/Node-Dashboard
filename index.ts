dd3276edbb2956108fdfdc63a02802de2a59a823
Omar <omarboulakchour@gmail.com>
2025-05-05 16:46:52 +0200

    añadir corse

--- index.ts
+++ index.ts
@@ -2,21 +2,25 @@ import dotenv from 'dotenv';
 import express from 'express'
 import serverless from 'serverless-http';
 import { userRouter } from './controllers/userController';
+import { bookingRouter } from './controllers/bookingControler';
 import { AuthController } from './controllers/authController';
 import { authenticateToken } from './midleware/midleware';
 import connectDB from './seed/connection';
+import cors from 'cors';
 
 dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
+app.use(cors())
+
 
 app.use('/api/v1/auth', AuthController);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/booking', authenticateToken, userRouter);
+app.use('/api/v1/users', authenticateToken, userRouter);
+app.use('/api/v1/booking', authenticateToken, bookingRouter);
 
 // export const handler = () =>{
 //     connectDB().then(() =>{serverless(app)})
 // }
 
-app.listen(3000)
+app.listen(3001)
